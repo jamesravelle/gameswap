335ac296ac4fd9e2820004db2a74a7b03a0f8fd3
jxrvll <jxrvll@gmail.com>
2022-02-27 16:48:30 -0500

    Update Mongo DB

--- index.js
+++ index.js
@@ -20,14 +20,6 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
 
-// mongo db
-const CONNECTION_URL = 'mongodb+srv://jravelle:Bnjygs23@cluster0.mc1if.mongodb.net/gameswap?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-        .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
-        .catch((error) => console.log(error.message))
-
 // Post routes
 app.use('/posts', postRoutes)
 
@@ -105,3 +97,11 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
+
+// mongo db
+const CONNECTION_URL = `mongodb+srv://jravelle:${MONGO_DB_PASS}@cluster0.mc1if.mongodb.net/gameswap?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+        .catch((error) => console.log(error.message))
